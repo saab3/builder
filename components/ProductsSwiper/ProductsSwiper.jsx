fddb19c8c2b08dc3a6fc03fdfd8cae2fa94a71f6
MahmoudSabea <msabea@v4tech.com>
2024-08-11 02:12:48 +0300

    .

--- components/ProductsSwiper/ProductsSwiper.jsx
+++ components/ProductsSwiper/ProductsSwiper.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default async function ProductsSwiper() {
 
-  const products = await fetch("https://bb-api.v4tech.com/api/v1/search/products/facets/category/3?pageNo=1&pageSize=10&sortBy=price&sortDir=desc&region=all", {headers:{"Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTg0NTUxOTIsInN0b3JlTmFtZSI6ImFyIn0.QSvkx-kX-AjeX8s6Nd4KRurzJbhLhB9pXrZMjzJKBFQ`}})
+  const products = await fetch("https://api-staging.ops.blackbox.com.sa/api/v1/search/products/facets/category/3?pageNo=1&pageSize=10&sortBy=price&sortDir=desc&region=all", {headers:{"Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTg0NTUxOTIsInN0b3JlTmFtZSI6ImFyIn0.QSvkx-kX-AjeX8s6Nd4KRurzJbhLhB9pXrZMjzJKBFQ`}})
   return (
-    <div>{products?.data?.products?.length}</div>
+    <div>{products?.data?.products?.length}</div> 
   )
 }
