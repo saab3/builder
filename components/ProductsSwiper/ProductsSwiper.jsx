87ad8a20b08360e2203ed3f2929f46ff506b402e
MahmoudSabea <msabea@v4tech.com>
2024-08-11 02:26:13 +0300

    .

--- components/ProductsSwiper/ProductsSwiper.jsx
+++ components/ProductsSwiper/ProductsSwiper.jsx
@@ -5,6 +5,7 @@ export default function ProductsSwiper() {
   useEffect(() => {
     return async () => {
       const res = await fetch("https://api-staging.ops.blackbox.com.sa/api/v1/search/products/facets/category/3?pageNo=1&pageSize=10&sortBy=price&sortDir=desc&region=all", {headers:{"Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjQ1Mzk5NzAsInN0b3JlTmFtZSI6ImVuIn0.cqTzXWyPPAP5NHpSX3UndP8mHa74ptZ9_WERa3VM03A`}})
+      console.log(res)
       setProducts(res)
     }
   }, [])
