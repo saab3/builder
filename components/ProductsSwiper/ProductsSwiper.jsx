f3a927f44bca47a67346f147c87dc3524ada6724
MahmoudSabea <msabea@v4tech.com>
2024-08-10 23:41:54 +0300

    .

--- components/ProductsSwiper/ProductsSwiper.jsx
+++ components/ProductsSwiper/ProductsSwiper.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default async function V4tech() {
+export default async function ProductsSwiper() {
   const token = `Bearer ${process.env.NEXT_PUBLIC_HANDSHAKE}`
 
   const products = await fetch("https://api.ops.blackbox.com.sa/api/v1/search/products/facets/category/3?pageNo=1&pageSize=10&sortBy=price&sortDir=desc&region=all", {headers:{"Authorization": token}})
---
+++ components/V4tech1/V4tech.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function V4tech() {
+  return (
+    <div>This is a V4tech custom component</div>
+  )
+}
