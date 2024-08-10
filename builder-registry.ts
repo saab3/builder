1c249f2947e40a1fd8c1cc27534b0ac040e29dab
MahmoudSabea <msabea@v4tech.com>
2024-08-10 23:52:30 +0300

    .

--- builder-registry.ts
+++ builder-registry.ts
@@ -1,6 +1,7 @@
 "use client";
 import { builder, Builder } from "@builder.io/react";
 import Counter from "./components/Counter/Counter";
+import ProductsSwiper from "./components/ProductsSwiper/ProductsSwiper";
 import V4tech from "./components/V4tech/V4tech";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
@@ -17,4 +18,8 @@ Builder.registerComponent(Counter, {
 
 Builder.registerComponent(V4tech, {
   name: "V4tech",
+});
+
+Builder.registerComponent(ProductsSwiper, {
+  name: "ProductsSwiper",
 });
--- components/V4tech1/V4tech.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function V4tech() {
-  return (
-    <div>This is a V4tech custom component</div>
-  )
-}
